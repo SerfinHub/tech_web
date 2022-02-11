2ea28b335a5786dfabeb68d710177661127a5ab5
seweryn123 <01114779@pw.edu.pl>
2022-02-11 19:26:52 +0100

    Updates

---
+++ precache-manifest.b519651513d8550b4263cbd32e08396d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7b03d4a75d5cbfdac0a740778e340f",
+    "url": "/tech_web/index.html"
+  },
+  {
+    "revision": "c2039b52df2e3f5bd784",
+    "url": "/tech_web/static/css/2.57e7574f.chunk.css"
+  },
+  {
+    "revision": "982573457f86cb503647",
+    "url": "/tech_web/static/css/main.03bab4b8.chunk.css"
+  },
+  {
+    "revision": "c2039b52df2e3f5bd784",
+    "url": "/tech_web/static/js/2.4ecae3d7.chunk.js"
+  },
+  {
+    "revision": "55bb4581f532d062aec3",
+    "url": "/tech_web/static/js/3.4f122290.chunk.js"
+  },
+  {
+    "revision": "982573457f86cb503647",
+    "url": "/tech_web/static/js/main.c83478af.chunk.js"
+  },
+  {
+    "revision": "b926647691cc812d202b",
+    "url": "/tech_web/static/js/runtime~main.9a53332d.js"
+  }
+]);
